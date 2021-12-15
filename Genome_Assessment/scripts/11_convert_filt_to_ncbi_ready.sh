5937651247475b35d38cabeab35d1e069af79c5a
Cassie Ettinger <casett@users.noreply.github.com>
2021-12-15 13:48:19 -0800

    Adding script to convert filtered files into NCBI ready files

---
+++ Genome_Assessment/scripts/11_convert_filt_to_ncbi_ready.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH -p batch,intel 
+#SBATCH --time 1-0:00:00 
+#SBATCH --ntasks 12 
+#SBATCH --nodes 1 
+#SBATCH --mem 24G 
+#SBATCH --out logs/convert.%a.log
+#SBATCH -J coel_genbank
+#SBATCH --mail-type=END # notifications for job done & fail
+#SBATCH --mail-user=cassande@ucr.edu # send-to address
+#SBATCH -D /rhome/cassande/shared/projects/Chytrid/Coelomomyces/Chytrid_Coelomomyces_Assessment/
+
+
+module unload miniconda2 miniconda3
+module load funannotate/1.8
+
+DIR=data/filtered
+SBTDIR=$DIR/sbt
+SAMPFILE=$DIR/samples.csv
+
+IFS=,
+tail -n +2 $SAMPFILE | sed -n ${N}p | while read PREFIX GFF FASTA SBT STRAIN
+do 
+	funannotate util gff2prot -g $DIR/$GFF -f $DIR/$FASTA > $DIR/$PREFIX.prot.fa
+	funannotate util gff2tbl -g $DIR/$GFF -f $DIR/$FASTA > $DIR/$PREFIX.tbl
+	funannotate util tbl2gbk -i $DIR/$PREFIX.tbl -f $DIR/$FASTA -s "Coelomomyces lativittatus" --strain $STRAIN --sbt $SBTDIR/$SBT --out $DIR/$PREFIX
+done
+
